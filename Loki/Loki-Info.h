8723b70b2e4990f4138b8d1ceb2f9f0e47cb3a7d
William Towe <willbur1984@gmail.com>
2020-08-26 02:52:31 -0400

    Update copyright

--- Loki/Loki-Info.h
+++ Loki/Loki-Info.h
@@ -3,7 +3,7 @@
 //  Loki
 //
 //  Created by William Towe on 3/9/17.
-//  Copyright (c) 2017 Kosoku Interactive, LLC. All rights reserved.
+//  Copyright (c) 2020 Kosoku Interactive, LLC. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
--- LokiDemo-iOS/ViewController.h
+++ LokiDemo-iOS/ViewController.h
@@ -3,7 +3,7 @@
 //  LokiDemo-iOS
 //
 //  Created by William Towe on 3/9/17.
-//  Copyright © 2019 Kosoku Interactive, LLC. All rights reserved.
+//  Copyright © 2020 Kosoku Interactive, LLC. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
